c987ae0a1493a0db6c833163f110cc2ac78744e7
Cahzello <rizkyrasya.m@gmail.com>
2023-01-19 08:08:11 +0700

    initial commit

---
+++ resources/views/profile.blade.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous"> --}}
+    <link rel="stylesheet" href="style.css">
+    <title>CV</title>
+</head>
+<body>
+    <div class="nama">
+        <h3>Rizky Rasya Maheswara</h3>
+    </div>
+    <div class="container">
+        <div class="img">
+            <img src="user.png" alt="UserPhoto" width="200px">
+        </div>
+        <div class="konten">
+            <h4>Saya adalah seorang programmer yang sedang belajar di bagian Frontend</h4>
+            <h4>Skill yang saya pelajari adalah berikut</h4>
+            <div class="skill">
+                <table>
+                    <tr>
+                        <th>Javascript</th>
+                        <th>Java</th>
+                    </tr>
+                    <tr>
+                        <th>HTML</th>
+                        <th>PHP</th>
+                    </tr>
+                    <tr>
+                        <th>CSS</th>
+                        <th>MySQL</th>
+                    </tr>
+                </table>
+            </div>
+
+            <h4>Skill-skill yang sudah saya pelajari masih materi dasar</h4>
+            <h4>Tapi saya sudah mengetrti bagaimana bahasa pemrogarman bekerja</h4>
+
+        </div>
+    </div>
+    
+    
+</body>
+</html>
