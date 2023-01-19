f805900dc2afaed7faf41d9d5fa1c304b227d325
Cahzello <rizkyrasya.m@gmail.com>
2023-01-19 08:33:11 +0700

    push ke 2

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -13,33 +13,57 @@
         <h3>Rizky Rasya Maheswara</h3>
     </div>
     <div class="container">
-        <div class="img">
-            <img src="user.png" alt="UserPhoto" width="200px">
-        </div>
-        <div class="konten">
-            <h4>Saya adalah seorang programmer yang sedang belajar di bagian Frontend</h4>
-            <h4>Skill yang saya pelajari adalah berikut</h4>
-            <div class="skill">
-                <table>
-                    <tr>
-                        <th>Javascript</th>
-                        <th>Java</th>
-                    </tr>
-                    <tr>
-                        <th>HTML</th>
-                        <th>PHP</th>
-                    </tr>
-                    <tr>
-                        <th>CSS</th>
-                        <th>MySQL</th>
-                    </tr>
-                </table>
+        <div class="kontainer">
+            <div class="konten">
+                <div class="img">
+                    <img src="user.png" alt="UserPhoto" width="200px">
+                </div>
+                <h4>Saya adalah seorang programmer yang sedang belajar di bagian Frontend</h4>
+                <h4>Skill yang saya pelajari adalah berikut</h4>
+                <div class="skill">
+                    <table>
+                        <tr>
+                            <th>Javascript</th>
+                            <th>Java</th>
+                        </tr>
+                        <tr>
+                            <th>HTML</th>
+                            <th>PHP</th>
+                        </tr>
+                        <tr>
+                            <th>CSS</th>
+                            <th>MySQL</th>
+                        </tr>
+                    </table>
+                </div>
+    
+                <h4>Skill-skill yang sudah saya pelajari masih materi dasar</h4>
+                <h4>Tapi saya sudah mengetrti bagaimana bahasa pemrogarman bekerja</h4>
+    
             </div>
-
-            <h4>Skill-skill yang sudah saya pelajari masih materi dasar</h4>
-            <h4>Tapi saya sudah mengetrti bagaimana bahasa pemrogarman bekerja</h4>
-
+            <div class="isi">
+                <h3>Keterampilan</h3>
+                <h4>Ada beberapa Keterampilan yang saya punya, yaitu</h4>
+                <div class="keterampilan">
+                    <table>
+                        <tr>
+                            <th>Saya mampu bekerja cepat</th>
+                        </tr>
+                        <tr>
+                            <th>Cepat Tanggap</th>
+                        </tr>
+                        <tr>
+                            <th>Bisa bekerja sama dalam team</th>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="tentang">
+                <h3>Tentang Saya</h3>
+                <h4>Nama saya adalah Rizky Rasya Maheswara, saya adalah pelajar SMK. Saya memilih jalur pemrograman karena saya suka dengan hal itu. Saya ingin mempunyai cita cita menjadi sorang Web Developer. Karena saya rasa menjadi Web Developer adalah hal yang seru.</h4>
+            </div>
         </div>
+        
     </div>
     
     
