7276f7a5d56557fc231a87d4be7c92ae65868a2f
Cahzello <rizkyrasya.m@gmail.com>
2023-01-26 07:27:38 +0700

    siswarpl

---
+++ resources/views/smkbpi/siswarpl.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Daftar Siswa</title>
+</head>
+<body>
+    <h1>Daftar Siswa</h1>
+    <ol>
+
+        <li><?php echo $siswa07; ?></li>
+    </ol>
+    <div>
+        Copyright &copy; <?php echo date("Y"); ?> Rizky Rasya
+    </div>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,18 @@ Route::get('/', function () {
 Route::get('profile', function () {
     return view('profile');
 });
+
+Route::get('/siswa', function () {
+    return view('smkbpi.siswarpl', [
+        "siswa01" => "Rizky rasya",
+        "siswa02" => "Kasumi Toyama",
+        "siswa03" => "Arisa Ichigaya",
+        "siswa04" => "Tae Hanazono",
+        "siswa05" => "Rimi Ushigome",
+        "siswa06" => "Saya Yamabuki"
+    ]);
+});
+
+Route::get('/siswarpl', function () {
+    return view('smkbpi.siswarpl')->with('siswa07', 'Yukina Minato');
+});
